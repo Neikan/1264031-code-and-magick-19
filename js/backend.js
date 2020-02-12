e128fa17dd18acebe373c8ed6b49ad0b28b9a11a
Neikan <sladkov.e.m@outlook.com>
2020-02-12 19:31:46 +0300

    Добавление возможности получения и отправки данных магов с сервера

---
+++ js/backend.js
@@ -0,0 +1,61 @@
+'use strict';
+
+(function () {
+  var URL_GET = 'https://js.dump.academy/code-and-magick/data';
+  var URL_POST = 'https://js.dump.academy/code-and-magick';
+
+  var TIMEOUT = 10000; // 10s
+
+  var StatusCode = {
+    Ok: 200,
+    NotFound: 404
+  };
+
+  window.load = function (successHandler, errorHandler) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+
+      switch (xhr.status) {
+        case (StatusCode.Ok):
+          successHandler(xhr.response);
+          break;
+        case (StatusCode.NotFound):
+          errorHandler('Сервер недоступен. Мы работаем, чтобы скорее все починить!');
+          break;
+        default:
+          errorHandler('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      errorHandler('Произошла ошибка соединения. Пожалуйста, проверьте сетевое подключение');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      errorHandler('Запрос не успел выполниться за ' + TIMEOUT + 'мс. Пожалуйста, проверьте качество сетевого подключения');
+    });
+
+    xhr.open('GET', URL_GET);
+
+    xhr.send();
+  };
+
+  window.save = function (data, successHandler, errorHandler) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      successHandler(xhr.response);
+    });
+
+    xhr.addEventListener('error', function () {
+      errorHandler('Произошла ошибка соединения. Пожалуйста, проверьте подключение');
+    });
+
+    xhr.open('POST', URL_POST);
+    xhr.send(data);
+  };
+
+})();
